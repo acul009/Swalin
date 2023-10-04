9752bdca4e4d449d1543d8729d6076cce5b4804b
L. Wlcek <luca@it-rahn.de>
2023-10-04 18:17:46 +0200

    revamped additional parameters for encryped data

--- util/crypto.go
+++ util/crypto.go
@@ -1,66 +1,102 @@
 package util
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"encoding/json"
+	"fmt"
 
 	"golang.org/x/crypto/argon2"
 )
+
+var encryptionInfoDelimiter = []byte("\n")
 
 // EncryptDataWithPassword encrypts data using a password and returns the encrypted result.
 func EncryptDataWithPassword(password []byte, data []byte) ([]byte, error) {
 	// Generate a random salt
 	salt := make([]byte, aes.BlockSize)
 	if _, err := rand.Read(salt); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed generating salt: %v", err)
+	}
+
+	iv := make([]byte, aes.BlockSize)
+	if _, err := rand.Read(iv); err != nil {
+		return nil, fmt.Errorf("failed generating iv: %v", err)
+	}
+
+	parameters := EncryptionParameters{
+		ArgonParameters: ArgonParameters{
+			ArgonOptions: DefaultArgonOptions(),
+			Salt:         salt,
+		},
+		IV: iv,
 	}
 
 	// Derive an encryption key from the password and salt
-	key, err := deriveKeyFromPassword(password, salt)
+	key, err := deriveKeyFromPassword(password, parameters.ArgonParameters)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed deriving encryption key: %v", err)
 	}
 
 	// Create a new AES cipher block
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed creating cipher: %v", err)
 	}
 
 	// Create a stream cipher for encryption
-	stream := cipher.NewCFBEncrypter(block, salt)
+	stream := cipher.NewCFBEncrypter(block, iv)
 
 	// Encrypt the data
 	encryptedData := make([]byte, len(data))
 	stream.XORKeyStream(encryptedData, data)
 
-	// Prepend the salt to the encrypted data
-	encryptedDataWithSalt := append(salt, encryptedData...)
+	// Prepend the parameters to the encrypted data
+	encryptionInfo, err := json.Marshal(parameters)
+	if err != nil {
+		return nil, fmt.Errorf("failed marshaling parameters: %v", err)
+	}
+	encryptionInfo = append(encryptionInfo, encryptionInfoDelimiter...)
 
-	return encryptedDataWithSalt, nil
+	dataToSave := append(encryptionInfo, encryptedData...)
+
+	return dataToSave, nil
 }
 
 // DecryptDataWithPassword decrypts data that was encrypted with a password.
 func DecryptDataWithPassword(password, encryptedData []byte) ([]byte, error) {
-	// Extract the salt from the beginning of the encrypted data
-	salt := encryptedData[:aes.BlockSize]
-	encryptedPayload := encryptedData[aes.BlockSize:]
+
+	// Extract the parameters from the beginning of the encrypted data
+	parts := bytes.SplitN(encryptedData, encryptionInfoDelimiter, 2)
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("failed splitting encrypted data")
+	}
+
+	encodedParams := parts[0]
+	encryptedPayload := parts[1]
+
+	parameters := &EncryptionParameters{}
+	err := json.Unmarshal(encodedParams, parameters)
+	if err != nil {
+		return nil, fmt.Errorf("failed unmarshaling parameters: %v", err)
+	}
 
 	// Derive the encryption key from the password and salt
-	key, err := deriveKeyFromPassword(password, salt)
+	key, err := deriveKeyFromPassword(password, parameters.ArgonParameters)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed deriving encryption key: %v", err)
 	}
 
 	// Create a new AES cipher block
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed creating cipher: %v", err)
 	}
 
 	// Create a stream cipher for decryption
-	stream := cipher.NewCFBDecrypter(block, salt)
+	stream := cipher.NewCFBDecrypter(block, parameters.IV)
 
 	// Decrypt the payload
 	decryptedData := make([]byte, len(encryptedPayload))
@@ -69,13 +105,34 @@ func DecryptDataWithPassword(password, encryptedData []byte) ([]byte, error) {
 	return decryptedData, nil
 }
 
-func deriveKeyFromPassword(password []byte, salt []byte) ([]byte, error) {
-	// Parameters for Argon2id (adjust according to your security requirements)
-	timeCost := 1           // Number of iterations
-	memoryCost := 64 * 1024 // Memory usage in KiB
-	parallelism := 4        // Number of threads
-	keyLen := 32            // Desired key length in bytes
+type ArgonOptions struct {
+	TimeCost    uint32
+	MemoryCost  uint32
+	Parallelism uint8
+	KeyLength   uint32
+}
 
-	key := argon2.IDKey(password, salt, uint32(timeCost), uint32(memoryCost), uint8(parallelism), uint32(keyLen))
+type ArgonParameters struct {
+	ArgonOptions
+	Salt []byte
+}
+
+type EncryptionParameters struct {
+	ArgonParameters
+	IV []byte
+}
+
+func DefaultArgonOptions() ArgonOptions {
+	return ArgonOptions{
+		TimeCost:    1,
+		MemoryCost:  64 * 1024,
+		Parallelism: 4,
+		KeyLength:   32,
+	}
+}
+
+func deriveKeyFromPassword(password []byte, params ArgonParameters) ([]byte, error) {
+
+	key := argon2.IDKey(password, params.Salt, uint32(params.TimeCost), uint32(params.MemoryCost), uint8(params.Parallelism), uint32(params.KeyLength))
 	return key, nil
 }
---
+++ util/crypto_test.go
@@ -0,0 +1,28 @@
+package util_test
+
+import (
+	"bytes"
+	"rahnit-rmm/util"
+	"testing"
+)
+
+func TestEncryptDataWithPassword(t *testing.T) {
+	password := []byte("password")
+
+	data := []byte("Hello, I'm some data that I want to encrypt!")
+
+	encryptedData, err := util.EncryptDataWithPassword(password, data)
+	if err != nil {
+		t.Fatalf("failed encrypting data: %v", err)
+	}
+
+	decryptedData, err := util.DecryptDataWithPassword(password, encryptedData)
+	if err != nil {
+		t.Fatalf("failed decrypting data: %v", err)
+	}
+
+	if !bytes.Equal(decryptedData, data) {
+		t.Fatalf("decrypted data does not match original data")
+	}
+
+}
