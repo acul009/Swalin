a959661d97c7eeddc37eccfa074625e852d0357a
wrong <acul009@gmail.com>
2024-01-27 00:41:33 +0100

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          go_1_21
+          gotools
+          glibc
+          pkg-config
+          gcc
+          libGL.dev
+          xorg.libX11.dev
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXinerama
+          xorg.libXi.dev
+          xorg.libXxf86vm
+          glfw
+        ];
+
+
+
+        shellHook = "codium;exit;";
+      };
+    };
+}
